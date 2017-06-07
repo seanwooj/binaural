f613e361d623647d8cea53935924e818ee37d842
Sean Kim <seanwoojinkim@gmail.com>
2017-06-06 17:34:36 -0700

    added a slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import BinauralTone from './BinauralTone'
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const ToolTipSlider = createSliderWithTooltip(Slider);
 
 class App extends Component {
+  state = {
+    baseHz:220,
+    beatHz: 12
+  }
+
   componentDidMount() {
-    this.bTone = new BinauralTone({baseHz: 220, beatHz: 12});
+    this.bTone = new BinauralTone({baseHz: this.state.baseHz, beatHz: this.state.beatHz});
   }
 
   startTone = () => {
@@ -15,9 +25,17 @@ class App extends Component {
     this.bTone.stop();
   }
 
+  updateBeatHz = (hz) => {
+    this.setState({
+      beatHz: hz
+    });
+    this.bTone.updateBeatHz(hz);
+  }
+
   render() {
     return (
       <div className='container'>
+        <ToolTipSlider min={0.1} max={40} step={0.1} value={this.state.beatHz} onChange={this.updateBeatHz}/>
         <button onClick={this.startTone}>Start</button>
         <button onClick={this.endTone}>Stop</button>
       </div>
--- src/BinauralTone.js
+++ src/BinauralTone.js
@@ -12,22 +12,43 @@ class BinauralTone {
     const rTone = new Tone.Oscillator(this.rightHz, 'sine').connect(lPan);
     const lTone = new Tone.Oscillator(this.leftHz, 'sine').connect(rPan);
 
-    this.tones = [rTone, lTone];
+    this.tones = [lTone, rTone];
 
-		var noise = new Tone.Noise("pink").start();
+		// var noise = new Tone.Noise("pink").start();
+		//
+		// //make an autofilter to shape the noise
+		// var autoFilter = new Tone.AutoFilter({
+		// 	"frequency" : 12,
+		// 	"depth" : 1,
+		// 	"baseFrequency" : 200,
+		// 	"octaves" : .05
+		// }).connect(Tone.Master);
+		//
+		// //connect the noise
+		// noise.connect(autoFilter);
+		// //start the autofilter LFO
+		// // autoFilter.start()
+  }
 
-		//make an autofilter to shape the noise
-		var autoFilter = new Tone.AutoFilter({
-			"frequency" : 12,
-			"depth" : 1,
-			"baseFrequency" : 200,
-			"octaves" : .05
-		}).connect(Tone.Master);
+	calculateLR = (baseHz, beatHz) => {
+		const l = baseHz - (beatHz / 2),
+					r = baseHz + (beatHz / 2);
 
-		//connect the noise
-		noise.connect(autoFilter);
-		//start the autofilter LFO
-		// autoFilter.start()
+		return [l, r];
+		// return [l, r]
+	}
+
+	updateBeatHz = (beatHz) => {
+		const toneHzLR = this.calculateLR(this.baseHz, beatHz);
+		console.log(beatHz);
+		this.tones.forEach((tone, i) => {
+			tone.frequency.value = toneHzLR[i];
+		});
+	}
+
+	updateBaseHz = (hz) => {
+		this.baseHz = hz;
+		this.updateBeatHz(this.beatHz);
   }
 
   start = () => {
@@ -37,14 +58,6 @@ class BinauralTone {
 	stop = () => {
 		this.tones.forEach((tone) => {tone.stop()});
 	}
-
-  updateBeatHz = () => {
-
-  }
-
-  updateBaseHz = () => {
-
-  }
 
   // this is the callback which should be called every time
   // the beatHz or baseHz are updated. but those should only be updated
